f1f0d8cbb97b0a241adcded372f34c2388ea4cf6
olivamat <matiasholeh@gmail.com>
2019-07-17 20:18:14 +0300

    Updates

---
+++ precache-manifest.683047c96e0368711a010683b2a1bdf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595a2ef8ab7a379e019266846315d983",
+    "url": "/goit-react-hw-03-pet-store/index.html"
+  },
+  {
+    "revision": "92fa3c9d3b3c42ddc730",
+    "url": "/goit-react-hw-03-pet-store/static/css/main.ad9f59de.chunk.css"
+  },
+  {
+    "revision": "bd1b09d9f356f37704a2",
+    "url": "/goit-react-hw-03-pet-store/static/js/2.bb8fb46d.chunk.js"
+  },
+  {
+    "revision": "92fa3c9d3b3c42ddc730",
+    "url": "/goit-react-hw-03-pet-store/static/js/main.40599c28.chunk.js"
+  },
+  {
+    "revision": "688b9ecaf7021802e591",
+    "url": "/goit-react-hw-03-pet-store/static/js/runtime~main.fc0dd55c.js"
+  }
+]);
